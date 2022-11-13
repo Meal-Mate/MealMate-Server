bb11e320aa34de818bce34a7d2e8cf4519f7617a
Hassen Mabrouk <hassen.mabrouk@esprit.tn>
2022-11-13 20:01:43 +0100

    Gestion Proposition
    Signed-off-by: Hassen Mabrouk <hassen.mabrouk@esprit.tn>

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import mongoose from 'mongoose'
 import adminRoutes from './Routes/admin.route.js'
 import userRoutes from './Routes/user.route.js'
 import restaurantRoutes from './Routes/restaurant.route.js'
+import propositionRoutes from './Routes/proposition.route.js'
 import passport from 'passport'
 import morgan from 'morgan'
 
@@ -16,6 +17,7 @@ const port = process.env.PORT || 9095
 app.use('/user', userRoutes)
 app.use('/admin', adminRoutes)
 app.use('/restaurant', restaurantRoutes)
+app.use('/proposition', propositionRoutes)
 mongoose.connect('mongodb://127.0.0.1/MealMateDB')
 
 const connection = mongoose.connection
